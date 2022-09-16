1642d2159d95bd79e4d707c7a7b9cbd69232a571
Ömür Yıldırım <omuryildirim@ymail.com>
2022-09-16 23:14:07 +0200

    Serve html file

--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-require('./mongodb');
 const router = require('./router');
 
 const app = express();
@@ -16,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use('/api', router);
 app.get('*', (req, res) => {
-  res.sendFile('build/index.html', {root: path.join(__dirname, '../')});
+  res.sendFile('public/index.html', {root: path.join(__dirname, '../')});
 });
 
 module.exports = app;

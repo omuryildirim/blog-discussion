8a2bd4bdcfbfdc468ce1ce25d5c4c1ba331e5703
Ömür Yıldırım <omuryildirim@ymail.com>
2022-09-17 15:58:31 +0200

    Add get users and get user endpoints

---
+++ server/routes/user.js
@@ -0,0 +1,30 @@
+const mockUserData = [{
+    name: "Anna Karenina",
+    _id: "6325c5e7c7fd8afeaff45fb3",
+    image: "anna.jpeg"
+}, {
+    name: "Aleksey Vronski",
+    _id: "6325c5e7c7fd8afeaff45fb4",
+    image: "vronski.jpeg"
+}, {
+    name: "Konstantin Levin",
+    _id: "6325c5e7c7fd8afeaff45fb5",
+    image: "levin.jpeg"
+}, {
+    name: "Darya Aleksandrovna",
+    _id: "6325c5e7c7fd8afeaff45fb6",
+    image: "darya.jpeg"
+}]
+
+
+module.exports = (router) => {
+  router.get('/user', (req, res) => {
+    const randomIndex = Math.ceil(Math.random() * 4) - 1;
+    const user = mockUserData[randomIndex];
+    res.status(200).send(user);
+  });
+
+  router.get('/users', (req, res) => {
+    res.status(200).send(mockUserData.reduce((list, user) => {list[user._id] = user; return list;}, {}));
+  });
+};

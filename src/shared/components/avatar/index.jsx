8399aa0b43cfa3f58eb2fee64ca3bed13fd8d843
Ömür Yıldırım <omuryildirim@ymail.com>
2022-09-20 01:01:05 +0200

    Unit test coverage for Avatar component

--- src/shared/components/avatar/index.jsx
+++ src/shared/components/avatar/index.jsx
@@ -4,7 +4,7 @@ import {Avatar} from '@mui/material';
 export const UserAvatar = ({image, name}) => {
     return (
         <>
-            <Avatar alt={name} src={`/public/images/${image}`} sx={{height: 34, width: 34}} />
+            <Avatar alt={name} src={`/public/images/${image}`} sx={{height: 34, width: 34}} data-testid="avatar" />
         </>
     );
 };

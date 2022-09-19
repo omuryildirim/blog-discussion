7629e5b1b002ff44d694bdbb309eead03869ecef
Ömür Yıldırım <omuryildirim@ymail.com>
2022-09-20 01:01:19 +0200

    Unit test coverage for shared helpers functions

---
+++ src/shared/utils/helpers.test.js
@@ -0,0 +1,18 @@
+import {updateComment} from "./helpers";
+import {mockComments} from "../../../__mocks__/comments";
+
+describe("helpers", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe("updateComment", () => {
+        it("should process update comment accordingly", () => {
+            const setComments = jest.fn();
+            const updatedComment = {...mockComments[0], upvotes: ["test"]};
+            updateComment({comments: mockComments, updatedComment, setComments} );
+
+            expect(setComments).toHaveBeenCalledWith([{...mockComments[0], upvotes: ["test"]}, ...mockComments.slice(1)]);
+        })
+    });
+});
